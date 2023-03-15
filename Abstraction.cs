25faa2d89ea79b220803eb409b6c20a34f5ff007
Ashwini <salunkheashwini1998@gmail.com>
2023-03-15 15:58:19 +0530

    abstraction concept

---
+++ Abstraction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConAssigment1
+{
+    //internal class Abstraction
+    abstract class Shape
+    { 
+        public abstract int area();
+    }
+    class Square : Shape
+    {
+        private int side;
+
+        public Square(int x = 0)
+        {
+            side = x;
+        }
+
+        public override int area()
+        {
+            Console.Write("Area of Square: ");
+            return (side * side);
+        }
+    }
+}
+
