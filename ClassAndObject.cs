fa70778923edd8211e4e8c496b6f0fe3f38ad8ba
Ashwini <salunkheashwini1998@gmail.com>
2023-03-15 16:05:44 +0530

    creat class and object

---
+++ ClassAndObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConAssigment1
+{
+    //internal class ClassAndObject
+ 
+        public class Car
+        {
+            public string name;
+            public double price()
+            {
+                return 80000;
+            }
+
+        }
+    }
+
+
+
