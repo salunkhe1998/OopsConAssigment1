d5e9e3181639ad4077bcabf84b86955a8ec42861
Ashwini <salunkheashwini1998@gmail.com>
2023-03-17 15:13:15 +0530

    single inharitance

---
+++ SingleInharitance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConAssigment1
+{
+    //internal class SingleInharitance
+    public class Animal
+    {
+        public void eat() { Console.WriteLine("Eating..."); }
+    }
+    public class Dog : Animal
+    {
+        public void bark() { Console.WriteLine("Barking..."); }
+    }
+    class TestInheritance2
+    {  
+    
+    }
+}
